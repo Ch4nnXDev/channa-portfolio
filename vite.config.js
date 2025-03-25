8e684722c5fd37876529973461e9ff51b2cbc56a
Channa2146@gmail.com <Channa2146@gmail.com>
2025-03-26 01:47:57 +0530

    resolve

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Portfolio" //same name as your repo
+  base:"/channa-portfolio" //same name as your repo
 })
